0cfb5f272e0526a1432b9ddfe93639d2f5545e70
derek <derekli1995@outlook.com>
2019-02-06 18:50:13 -0500

    empty

--- client/src/_components/GamesContent/GamesContent.jsx
+++ client/src/_components/GamesContent/GamesContent.jsx
@@ -14,7 +14,7 @@ class GameCards extends Component {
                 <Card.Group itemsPerRow={4}>
                         {this.props.games.map((g) =>
                             <Card key={g.gameName}>
-                                <Image src={g.imgIconUrl} />
+                                <Image src={g.imgLogoUrl=="" ? "" : g.imgLogoUrl} />
                                 <Card.Content>
                                 <Card.Header>{g.gameName}</Card.Header>
                                 </Card.Content>
